28e6a13b8aacff41d7c2686693a5e87f290cd72c
佐藤太一 <3101taichi@gmail.com>
2023-04-01 23:04:52 +0900

    create_coffee_store

--- app/graphql/mutations/create_coffee_store.rb
+++ app/graphql/mutations/create_coffee_store.rb
@@ -1,14 +1,25 @@
 module Mutations
   class CreateCoffeeStore < BaseMutation
-    # TODO: define return fields
-    # field :post, Types::PostType, null: false
+    argument :name, String, required: true
+    argument :location, String, required: false
+    argument :online_store_url, String, required: false
 
-    # TODO: define arguments
-    # argument :name, String, required: true
+    field :coffee_store, Types::CoffeeStoreType, null: false
+    field :errors, [String], null: false
 
-    # TODO: define resolve method
-    # def resolve(name:)
-    #   { post: ... }
-    # end
+    def resolve(name:, location: nil, online_store_url: nil)
+      coffee_store = CoffeeStore.new(name: name, location: location, online_store_url: online_store_url)
+      if coffee_store.save
+        {
+          coffee_store: coffee_store,
+          errors: []
+        }
+      else
+        {
+          coffee_store: nil,
+          errors: coffee_store.errors.full_messages
+        }
+      end
+    end
   end
 end
