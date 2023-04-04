09ec4279f116d129596dcdbbc9581ca6cae908e0
佐藤太一 <3101taichi@gmail.com>
2023-04-04 23:51:21 +0900

    サインイン状態でのみ、コーヒー店を作成するように

--- app/graphql/mutations/create_coffee_store.rb
+++ app/graphql/mutations/create_coffee_store.rb
@@ -4,10 +4,19 @@ module Mutations
     argument :location, String, required: false
     argument :online_store_url, String, required: false
 
-    field :coffee_store, Types::CoffeeStoreType, null: false
+    field :coffee_store, Types::CoffeeStoreType, null: true
     field :errors, [String], null: false
 
     def resolve(name:, location: nil, online_store_url: nil)
+      user = context[:current_user]
+      # サインイン状態でない場合はエラーを返す
+      if user.nil?
+        return {
+          coffee_store: nil,
+          errors: ["ログインが必要です"]
+        }
+      end
+      
       coffee_store = CoffeeStore.new(name: name, location: location, online_store_url: online_store_url)
       if coffee_store.save
         {
